5f06f8a61f6bba4d82c0bfd1875c82c5f822b5da
Nguyen Quoc Hung <kent@Nguyens-MacBook-Air-2.local>
2025-12-26 13:12:27 +0700

    refactor(pom): enhance BasePage with common behavior

--- src/pages/BasePage.ts
+++ src/pages/BasePage.ts
@@ -1,9 +1,13 @@
 import { Page } from '@playwright/test';
 
-export class BasePage {
-  protected page: Page;
+export abstract class BasePage {
+  protected readonly page: Page;
 
   constructor(page: Page) {
     this.page = page;
+  }
+
+  protected async waitForPageReady() {
+    await this.page.waitForLoadState('domcontentloaded');
   }
 }
